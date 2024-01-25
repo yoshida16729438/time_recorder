27099b398e523c43629c78ecc61cedca6ab21e11
yoshida16729438 <yoshida16729438@gmail.com>
2024-01-25 09:43:05 +0900

    同時編集防止関連の削除漏れを修正

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,7 +1,6 @@
 import { FC, useState } from "react";
 
 import { CodeContextProvider } from "../../providers/CodeProvider";
-import { EditingProvider } from "../../providers/EditStatusProvider";
 import { Time } from "../../utils/timeUtil";
 import Header from "../templates/Header";
 import WorkStartTimeOrgan from "../organisms/WorkStartTimeOrgan";
@@ -39,32 +38,30 @@ const Main: FC = () => {
 
   return (
     <CodeContextProvider>
-      <EditingProvider>
-        <Header />
-        <Container>
-          <h1>勤務開始時刻</h1>
-          <WorkStartTimeOrgan
-            workStartTime={startTime}
-            onSetWorkStartTime={onSetWorkStartTime}
-          />
-          <hr />
-          <h1>業務切替ログ</h1>
-          <h2>レコード追加</h2>
-          <AddTimeRecordOrgan
-            lastRecordedTime={lastRecordedTime}
-            onAddRecord={onAddRecord}
-          />
-          <h2>ログ</h2>
-          <TimeRecordLogOrgan
-            workStartTime={startTime}
-            records={timeRecords}
-            onEditRecord={onEditRecord}
-          />
-          <hr />
-          <h1>工数集計</h1>
-          <TotalTimeOrgan records={timeRecords} workStartTime={startTime} />
-        </Container>
-      </EditingProvider>
+      <Header />
+      <Container>
+        <h1>勤務開始時刻</h1>
+        <WorkStartTimeOrgan
+          workStartTime={startTime}
+          onSetWorkStartTime={onSetWorkStartTime}
+        />
+        <hr />
+        <h1>業務切替ログ</h1>
+        <h2>レコード追加</h2>
+        <AddTimeRecordOrgan
+          lastRecordedTime={lastRecordedTime}
+          onAddRecord={onAddRecord}
+        />
+        <h2>ログ</h2>
+        <TimeRecordLogOrgan
+          workStartTime={startTime}
+          records={timeRecords}
+          onEditRecord={onEditRecord}
+        />
+        <hr />
+        <h1>工数集計</h1>
+        <TotalTimeOrgan records={timeRecords} workStartTime={startTime} />
+      </Container>
     </CodeContextProvider>
   );
 };
